bdbde089d5b70c98e9390098100d1d14b553fff8
wangsaisai <shanbencilang@gmail.com>
2020-07-24 21:23:13 +0800

    Initial commit

---
+++ src/main/java/com/wss/redis/RedisApplication.java
@@ -0,0 +1,15 @@
+package com.wss.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wss
+ * @date 2020
+ */
+@SpringBootApplication
+public class RedisApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(RedisApplication.class, args);
+  }
+}
